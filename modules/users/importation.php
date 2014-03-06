630d54d807a419015f4f729426ed84b52493cebb
Elhocine TAKOUERT <el-hocine_takouert@etu.u-bougogne.fr>
2014-03-06 17:45:57 +0100

    Changement d'emplacement
    J'ai remonter les fichiers contenu dans des dossiers includes pour que
    index.php fonctionne
    On reverra après l'utilité ou pas des dossiers includes

--- modules/users/importation.php
+++ modules/users/importation.php
@@ -3,9 +3,6 @@
   // Inclusion du modèle importation
   include_once CHEMIN_MODELE.'importation_modele.php';
 
-  //Récupération login utilisateur
-  $info_user = get_information_user($_SESSION['id_user']);
-
   // Appel de la vue
   include_once CHEMIN_VUE.'importation_vue.php';
 
